f6da5cfaa9c292bc101fd8c2a60968d189b7a33e
andrei <andrei.fzto@gmail.com>
2016-01-30 15:57:15 +0000

    +

--- app.js
+++ app.js
@@ -3,22 +3,19 @@
 const http = require('http')
 const url  = require('url')
 const port = process.env.PORT || 3000
-const hostname = process.env.IP
+const hostname = process.env.IP || 'localhost'
 const timestamp = require('./timestamp')
 
 http.createServer((req,res) => {
-    if (req.method === 'GET') {
+  
+  if (req.method === 'GET') {
         
-        let path = url.parse(req.url)
-            .pathname
-            .slice(1)
-            .replace(/%20/g, ' ')
+    let path = url.parse(req.url)
+      .pathname
+      .slice(1)
+      .replace(/%20/g, ' ')
             
-        res.writeHead(200, {
-            'Content-Type': 'application/json'
-            })       
-            
-        res.end(path && JSON.stringify(timestamp(path)))
-    }
-    
+    res.writeHead(200, { 'Content-Type': 'application/json' })       
+    res.end(path && JSON.stringify(timestamp(path)))
+  }
 }).listen(port, hostname)
